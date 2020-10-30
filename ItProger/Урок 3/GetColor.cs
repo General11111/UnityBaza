fbd74d9ee5ed944361655b3b74dff3c8a94dd622
General11111 <6088623@gmail.com>
2020-10-30 16:02:12 +0300

    Добавил скрипты Уроки 2,3

---
+++ ItProger/Урок 3/GetColor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetColor : MonoBehaviour
+{
+    public GameObject obj;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            obj.SetActive(false);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Destroy(obj);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            obj.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else if (Input.GetKeyUp(KeyCode.G))
+        {
+            obj.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if (Input.GetKeyUp(KeyCode.B))
+        {
+            obj.GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
