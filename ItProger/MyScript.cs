bb65de4d328db9294f4a318d44858f6de2c3e765
General11111 <6088623@gmail.com>
2020-10-30 11:47:07 +0300

    Написание функции
    [SerializeField]
    private float abc = 23.45f;
    отображает приватную переменную в юнити на экране

---
+++ ItProger/MyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    [SerializeField]
+    private float abc = 23.45f;
+    void Start()
+    {
+        Show();
+    }
+
+    
+    void Show()
+    {
+        print(abc);
+    }
+}
